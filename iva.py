7e39870601daa3299c06859365f7815b8c3e5b63
julife12 <julian.jaimes.2019@upb.edu.cp>
2019-08-16 21:47:42 -0500

    mi tercer commit

---
+++ iva.py
@@ -0,0 +1,6 @@
+print("Hola bienvenido a el software para haya el valor de un producto con iva")
+p1=int(input("ingrese el valor del producto"))
+iva=float(input("ingrese el valor del iva"))
+valor=p1*iva/100
+productoV=p1+valor
+print("el valor del producto con iva es: ", productoV)
