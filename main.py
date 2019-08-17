3d677c9b200304eaeab51bd16afbe400b4294b7b
julife12 <julian.jaimes.2019@upb.edu.cp>
2019-08-16 20:33:27 -0500

    mi primer commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("holaaaa")
+a= int(input("pongame el primer numero:  "))
+b= int(input("ahora el segundo:  "))
+resultado=a*b
+print("el resultado de el producto es: ", resultado)
+doble=a*2
+print("el doble del primer numero: ", doble)
